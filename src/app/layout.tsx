f3661cf8f0c228dbb76c556856bc9d267d783ed4
Raijin1302 <raijinnguyen1302@gmail.com>
2023-06-19 00:00:58 +0700

    :hammer: add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,18 @@
-import '@/styles/globals.css'
-
+import { cn } from "@/lib/utils"
+import "@/styles/globals.css"
+import { Jost } from "next/font/google"
 export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
+  title: "Breaddit",
+  description: "A Reddit clone built with Next.js and TypeScript.",
 }
-
+const jost = Jost({ subsets: ["latin"] })
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
+    <html lang="en" className={cn()}>
       <body>{children}</body>
     </html>
   )
