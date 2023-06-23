85edc353228e658b0453e9d8404a3ec249699b6c
Raijin1302 <raijinnguyen1302@gmail.com>
2023-06-23 22:48:40 +0700

    :hammer: :red_circle: build UserAuthForm

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -13,7 +13,6 @@ const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
   const loginWithGoogle = async () => {
     setIsLoading(true)
     try {
-      throw new Error()
       await signIn("google")
     } catch (error) {
       toast({
@@ -25,11 +24,40 @@ const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
       setIsLoading(false)
     }
   }
+
+  const loginWithGithub = async () => {
+    setIsLoading(true)
+    try {
+      await signIn("github")
+    } catch (error) {
+      toast({
+        title: "There was a problem.",
+        description: "There was a error login Github",
+        variant: "destructive",
+      })
+    } finally {
+      setIsLoading(false)
+    }
+  }
   return (
-    <div className={cn("flex justify-center", className)} {...props}>
-      <Button onClick={loginWithGoogle} size="sm" className="w-full">
+    <div className={cn("flex justify-center gap-5", className)} {...props}>
+      <Button
+        onClick={loginWithGoogle}
+        isLoading={isLoading}
+        size="sm"
+        className="w-full"
+      >
         {isLoading ? null : <Icons.google className="h-4 w-4 mr-2" />}
         Google
+      </Button>
+      <Button
+        onClick={loginWithGithub}
+        isLoading={isLoading}
+        size="sm"
+        className="w-full"
+      >
+        {isLoading ? null : <Icons.github className=" h-4 w-4 mr-2" />}
+        Github
       </Button>
     </div>
   )
