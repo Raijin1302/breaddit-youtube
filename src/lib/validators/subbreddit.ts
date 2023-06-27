afbd8580904c315a3a1e89f5547ce58d3bd1496c
Raijin1302 <raijinnguyen1302@gmail.com>
2023-06-27 23:11:05 +0700

    :green_circle: subbreaddit type validator with zod

---
+++ src/lib/validators/subbreddit.ts
@@ -0,0 +1,12 @@
+import { z } from "zod"
+export const SubBreadditValidator = z.object({
+  name: z.string().min(3).max(21),
+})
+export const SubBredditSubscriptionValidator = z.object({
+  subBredditId: z.string(),
+})
+
+export type CreateSubBreadditPayload = z.infer<typeof SubBreadditValidator>
+export type SubscribeToSubBreadditPayload = z.infer<
+  typeof SubBredditSubscriptionValidator
+>
