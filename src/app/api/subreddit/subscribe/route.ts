df8357b7d3aa0814f0942d4f2b130bfb673a43a4
Raijin1302 <raijinnguyen1302@gmail.com>
2023-07-02 23:19:30 +0700

    :green_circle: create api route endpoint subscribe

---
+++ src/app/api/subreddit/subscribe/route.ts
@@ -0,0 +1,45 @@
+import { getAuthSession } from "@/lib/auth"
+import { db } from "@/lib/db"
+import { SubBredditSubscriptionValidator } from "@/lib/validators/subbreddit"
+import { z } from "zod"
+
+export async function POST(req: Request) {
+  try {
+    const session = await getAuthSession()
+    if (!session?.user) {
+      return new Response("Unauthorized", { status: 401 })
+    }
+    const body = await req.json()
+    const { subredditId } = SubBredditSubscriptionValidator.parse(body)
+
+    const subBreadditExist = await db.subscription.findFirst({
+      where: {
+        subredditId,
+        userId: session.user.id,
+      },
+    })
+
+    if (subBreadditExist) {
+      return new Response("You're already subscribed to this subreddit", {
+        status: 409,
+      })
+    }
+
+    await db.subscription.create({
+      data: {
+        subredditId,
+        userId: session.user.id,
+      },
+    })
+
+    return new Response(subredditId)
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response("Invalid request data passed", { status: 422 })
+    }
+
+    return new Response("Could not subscribe , please try again later", {
+      status: 500,
+    })
+  }
+}
