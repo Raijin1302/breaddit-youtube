2ebfadeddb1a2bcdf207524007ba48079f9e6ab7
Raijin1302 <raijinnguyen1302@gmail.com>
2023-07-25 23:57:36 +0700

    :hammer: :green_circle: build SearchBar & create api route search

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { Icons } from "./Icons"
 import { buttonVariants } from "./ui/Button"
 import { getAuthSession } from "@/lib/auth"
 import UserAccountNav from "./UserAccountNav"
+import SearchBar from "./SearchBar"
 
 const Navbar = async () => {
   const session = await getAuthSession()
@@ -17,7 +18,7 @@ const Navbar = async () => {
           </p>
         </Link>
         {/* Search Bar */}
-
+        <SearchBar />
         {session ? (
           <UserAccountNav user={session.user} />
         ) : (
