7624b52150d0912240892bf177702b0c70e0038c
Raijin1302 <raijinnguyen1302@gmail.com>
2023-07-27 17:44:11 +0700

    :hammer: fix lint & adding some issues searchbar

--- src/components/PostFeed.tsx
+++ src/components/PostFeed.tsx
@@ -7,6 +7,7 @@ import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config"
 import axios from "axios"
 import { useSession } from "next-auth/react"
 import Post from "./Post"
+import { Loader2 } from "lucide-react"
 interface PostFeedProps {
   initialPost: ExtendedPost[]
   subredditName?: string
@@ -80,10 +81,16 @@ const PostFeed: FC<PostFeedProps> = ({ initialPost, subredditName }) => {
               commentAmt={post.comments.length}
               post={post}
               subredditName={post.subreddit.name}
+              currentVote={currentVote}
             />
           )
         }
       })}
+      {isFetchingNextPage && (
+        <li className="flex justify-center">
+          <Loader2 className="w-6 h-6 animate-spin text-zinc-500" />
+        </li>
+      )}
     </ul>
   )
 }
